827deb77e05602b8a0f4e074c04964ce78e2a15a
Carolaine Alexandre <carolaine.ferrazemp@gmail.com>
2024-03-09 22:40:53 -0300

    Feat - Function Created onClick

--- components/ImageOverlayComponent.tsx
+++ components/ImageOverlayComponent.tsx
@@ -1,19 +1,28 @@
 import React, { ReactNode } from 'react';
-import { Image, ImageSourcePropType, View, StyleSheet, Text } from 'react-native';
+import { Image, ImageSourcePropType, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 interface IImage {
   imageUrl: ImageSourcePropType | undefined;
-  legend: ReactNode
+  legend: ReactNode;
+  onClick?: () => void; // Função opcional a ser chamada ao clicar
 }
 
-export default function ImageOverlayComponent({ imageUrl, legend }: IImage) {
+export default function ImageOverlayComponent({ imageUrl, legend, onClick }: IImage) {
+  const handlePress = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Image source={imageUrl} style={styles.imageStyles} />
-      <View style={styles.overlay}>
-        <Text style={styles.caption}>{legend}</Text>
+    <TouchableOpacity onPress={handlePress}>
+      <View style={styles.container}>
+        <Image source={imageUrl} style={styles.imageStyles} />
+        <View style={styles.overlay}>
+          <Text style={styles.caption}>{legend}</Text>
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 
@@ -21,7 +30,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    paddingTop: 50,
+    paddingTop: 10,
   },
 
   imageStyles: {
@@ -37,18 +46,18 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     left: 0,
-    marginTop: '50%',
+    marginTop: '40%',
     marginLeft: '5%',
     marginRight: '5%',
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
     padding: 10,
-    borderBottomLeftRadius: 20, 
-    borderBottomRightRadius: 20, 
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
 
   caption: {
     fontSize: 16,
-    color: '#fff', 
+    color: '#fff',
     textAlign: 'left',
     marginTop: '5%',
     marginLeft: '2%',
