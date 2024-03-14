e730fa59bf5959421eceefa86292978e0d0ff8d3
Ana Beatriz dos Santos Dantas <115746093+AnaBeatrizDosSantosDantas@users.noreply.github.com>
2024-03-13 21:19:18 -0300

    Update home.tsx

--- app/home.tsx
+++ app/home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import DrawerComponent from '../components/DrawerComponent';
 import ImageComponent from '../components/ImageComponent';
 
 export default function Home() {
@@ -9,13 +8,11 @@ export default function Home() {
     const imageUrl2 = require('../assets/image3.png');
 
     return (
-        <DrawerComponent>
             <View>
                 <ImageComponent imageUrl={imageUrl} descrição="Velas aromáticas" descrição1="" />
                 <ImageComponent imageUrl={imageUrl1} descrição="Velas normais" descrição1="" />
                 <ImageComponent imageUrl={imageUrl2} descrição="Velas aromáticas" descrição1="" />
                 {/* Conteúdo da tela Home */}
             </View>
-        </DrawerComponent>
     );
 }
