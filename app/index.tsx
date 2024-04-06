9584c0ac218ae6c410db2010a1337287bd66cf85
barbara-bruzon <bruzon.barbara@outlook.com>
2024-04-06 10:31:01 -0300

    personalizar imagens no home

--- app/index.tsx
+++ app/index.tsx
@@ -7,23 +7,30 @@ import { router } from 'expo-router';
 
 export default function Home() {
     const imageUrl = require('../assets/image1.png');
-    const imageUrl1 = require('../assets/image2.png');
+    const imageUrl1 = require('../assets/index1.png');
+    const imageUrl2 = require('../assets/index2.png');
+    const imageUrl3 = require('../assets/index3.png');
+    const imageUrl4 = require('../assets/index4.png');
     return(
             <ScrollView >
                 <Title title={'Novidades'}></Title>
-                <ImageOverlayComponent imageUrl={imageUrl} legend={"Exemplo de Legenda"}/>
+                <ImageOverlayComponent imageUrl={imageUrl} legend={"As velas de baunilha são a nova sensação!"}/>
                 <Title title={'Outros Tópicos'}></Title>
-                <ContainerImageText imageUrl={imageUrl1} text={'Como decidir o melhor aroma?'}
-                onPress={() => {
-                    router.push(`/aromas`)
-                }}/>
                 <ContainerImageText imageUrl={imageUrl1} text={'Conheça nosso portfólio!'}
                 onPress={() => {
                     router.push(`/portfolio`)
                 }}/>
-                <ContainerImageText imageUrl={imageUrl1} text={'Os segredos'}
+                <ContainerImageText imageUrl={imageUrl2} text={'Como escolher o melhor aroma?'}
+                onPress={() => {
+                    router.push(`/aromas`)
+                }}/>
+                <ContainerImageText imageUrl={imageUrl3} text={'Segredos das velas aromáticas'}
                 onPress={() => {
                     router.push(`/aromaDetalhe3`)
+                }}/>
+                <ContainerImageText imageUrl={imageUrl4} text={'Perguntas frequentes'}
+                onPress={() => {
+                    router.push(`/faq`)
                 }}/>
             </ScrollView>
     );
